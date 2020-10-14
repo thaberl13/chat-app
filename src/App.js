664bb0b55a86301c82f598275cbacf9fcfb591f0
Tanner Haberl <thaberl13@gmail.com>
2020-10-14 22:44:43 +0900

    added message, sign in, sign out, and chatroom components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import { useAuthState } from 'react-firebase-hooks';
+import { useCollectionData } from 'react-firebase-hooks';
+
+firebase.initializeApp({
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
 
 function App() {
+const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+       <h1>Chat App</h1>
       </header>
-    </div>
-  );
+
+  </div>
+  )
 }
 
 export default App;
---
+++ src/Components/ChatMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChatMessage() {
+  return (
+    <div>
+      <h1>Chat Message</h1>
+    </div>
+  )
+}
