b10b73617d446b2a2d27eea1be35eb2445f4d5d3
Tanner Haberl <thaberl13@gmail.com>
2020-10-15 10:25:10 +0900

    updated read me

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import React from "react";
 import "./App.css";
 import { useEffect, useRef, useState } from "react";
-import firebase from 'firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import SignIn from '../src/Components/SignIn';
-import ChatRoom from '../src/Components/Chatroom';
-import SignOut from '../src/Components/SignOut'
-require('firebase/auth')
-
-
+import firebase from "firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import SignIn from "../src/Components/SignIn";
+import ChatRoom from "../src/Components/Chatroom";
+import SignOut from "../src/Components/SignOut";
+require("firebase/auth");
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-
-
 function App() {
-const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
   return (
     <div className="App">
       <header>
-       <h1>Chat App</h1>
+        <h1>Chat App</h1>
       </header>
 
       <section>
-        {user ? <ChatRoom auth={auth} /> : <SignIn user={user}/>}
-        <SignOut/>
+        {user ? <ChatRoom auth={auth} /> : <SignIn user={user} />}
+        <SignOut />
       </section>
-
-  </div>
-  )
+    </div>
+  );
 }
 
 export default App;
