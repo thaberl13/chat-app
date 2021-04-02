4f233fa22350c77958c7e41c39025820a920e4a1
Tanner Haberl <thaberl13@gmail.com>
2021-04-01 17:32:47 -0700

    created folder for chat message

--- src/Components/ChatMessage/ChatMessage.jsx
+++ src/Components/ChatMessage/ChatMessage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { auth } from "firebase";
-import currentUser from "./Chatroom.jsx";
+import currentUser from "../Chatroom.jsx";
 
 export default function ChatMessage(props, { auth, currentUser }) {
   const { text, uid, photoUrl } = props.message;
